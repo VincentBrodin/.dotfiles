ff4e2e71ea11f3175d029692a87d82c89dcdb6fa
Vincent Brodin <vincent.brodin21@gmail.com>
2025-12-16 01:35:09 +0100

    Modern update

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Dotfiles devshell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.taplo
+          pkgs.hyprls
+          pkgs.vscode-css-languageserver
+        ];
+
+        shellHook = ''
+        '';
+      };
+    };
+}
